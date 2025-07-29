f43ac2585a5c23eadb45d72b06a3479d702efd85
Yevhen Kolomieits <Kolomieits@gmail.com>
2025-07-29 21:31:44 +0100

    blended-js-start

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,9 @@
-function slugify(title) {
-  return title.toLowerCase().split(' ').join('-');
-}
+const number = prompt('what is your name');
 
-console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+if (number === 10) {
+  alert('corect');
+  пше;
+} else {
+  alert('no correct');
+}
+console.log(number);
